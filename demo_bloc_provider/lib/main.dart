ab5d1068785ad2ab5e2dccad80662e490901efb2
Huu Bao Nguyen <huubao2309@gmail.com>
2020-02-17 19:49:50 +0700

    Provider

---
+++ demo_bloc_provider/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:demo_bloc_provider/provider/demo_future_provider.dart';
+import 'package:flutter/material.dart';
+
+// Reference: https://www.youtube.com/watch?v=TyMerwKXm30
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Demo Provider"),
+        ),
+        body: DemoFutureProvider(),
+      ),
+    );
+  }
+}
---
+++ demo_bloc_provider/lib/provider/basic.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+// Exposing a value
+class User {
+  // Không extends or with ChangeNotifer
+  String _name;
+
+  String get name => _name;
+  set name(newName) => _name = newName;
+}
+
+class BasicProvider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var user = User();
+    user.name = "Ryan Nguyen";
+
+    return Provider<User>.value(
+      value: user,
+      child: BasicWidget(),
+    );
+  }
+}
+
+class BasicWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          DemoConsumerWidget(),
+          DemoWithoutConsumerWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class DemoConsumerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Cách 1: use Consumer
+    return Consumer<User>(
+      builder: (context, user, child) => Text(user.name),
+    );
+  }
+}
+
+class DemoWithoutConsumerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Cach 2: Use Provider.of()
+    User user = Provider.of<User>(context);
+    return Container(
+      child: Text(user.name),
+    );
+  }
+}
